e92869cb6df26ec817156644cfe4b96e4c32e9d0
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2024-01-09 16:40:02 +0200

    feat: set blog, authors and tags collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+
+import tailwind from "@astrojs/tailwind";
+import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [react(), markdoc(), tailwind(), keystatic()],
+	output: "hybrid",
+});
