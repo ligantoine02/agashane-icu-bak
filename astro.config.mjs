8c06d645c1393e170845f012318cb153385bfb39
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2024-01-10 02:27:41 +0200

    fix: setup vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
-
 import tailwind from "@astrojs/tailwind";
 import keystatic from "@keystatic/astro";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), markdoc(), tailwind(), keystatic()],
-	output: "hybrid",
+  integrations: [react(), markdoc(), tailwind(), keystatic()],
+  output: "hybrid",
+  adapter: vercel()
 });
